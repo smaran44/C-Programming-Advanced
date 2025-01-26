2009621ed695bd2cbeddc11c16762d22917b1fab
Smaran Yanapu <smaranyanapu@gmail.com>
2025-01-26 17:14:42 +0530

    Update 08_recursion.c

--- 08_recursion.c
+++ 08_recursion.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 
 // Function to calculate the sum of the first n natural numbers using recursion
-int sumOfNaturalNumbers(int n) {
-    // Base case: The sum of the first 0 numbers is 0
-    if (n == 0) {
-        return 0; // Stop the recursion and return 0
-    }
 
-    // Recursive case: Sum of n is n + sum of the first (n-1) numbers
-    return n + sumOfNaturalNumbers(n - 1);
-}
+int sumOfNaturalNumbers(int n);
 
 int main() {
     int n;
@@ -23,4 +16,15 @@ int main() {
     printf("The sum of the first %d natural numbers is: %d\n", n, sum);
 
     return 0;
+}
+
+int sumOfNaturalNumbers(int n) {
+    // Base case: The sum of the first 0 numbers is 0
+    if (n == 0) {
+        return 0; // Stop the recursion and return 0
+    }
+
+    // Recursive case: Sum of n is n + sum of the first (n-1) numbers
+     int sumnm1 = sumOfNaturalNumbers(n - 1);
+     return sumnm1 + n;
 }
